261f394ff1a08bd8c915741b27d3a0b46b827100
omar eduardo <omar_aom_999@hotmail.com>
2016-10-27 22:40:44 -0500

    commit end

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,18 +1,24 @@
 class Inventory < ActiveRecord::Base
+	after_destroy :msgDelete
+
   belongs_to :category
 
-  has_one :work, dependent: :delete_all 
+  has_one :work, dependent: :delete
 
   validates :size, numericality: {greater_than: 0}
 
   scope :tam_x, ->(x){where(:size => x )}
  
- # validates :valid_size
+ # validate :valid_size
   
  # def valid_size
  # 	if self.size.nil? || self.size <= 0
  # 		errors.add(:size, 'must be greater than 0')
  #	end  
  #end
+
+ 	def msgDelete
+ 		Log.create(description: "La pieza con serial #{self.serial} ha sido borrado")
+ 	end
 
 end
